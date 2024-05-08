a9bde92527e01e6d117e55d5723cca70a6226d42
Bright <brightlsom@gmail.com>
2024-05-08 17:32:14 -0500

    Added a for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Second line in our file");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i);
+        }
     }
 }
